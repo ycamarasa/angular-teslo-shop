609a2f722f4d4c6283cc52ce4cc3cea365befc65
Maysel <y.camarasa@gmail.com>
2025-05-13 14:56:41 +0200

    Added footer

--- src/app/store-front/layouts/store-front-layout/store-front-layout.component.ts
+++ src/app/store-front/layouts/store-front-layout/store-front-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FrontNavbarComponent } from "../../components/frontNavbar/frontNavbar.component";
+import { FooterComponent } from '../../components/footer/footer/footer.component';
 
 @Component({
   selector: 'app-store-front-layout',
-  imports: [RouterOutlet, FrontNavbarComponent],
+  imports: [RouterOutlet, FrontNavbarComponent, FooterComponent],
   templateUrl: './store-front-layout.component.html',
 })
 export class StoreFrontLayoutComponent { }
